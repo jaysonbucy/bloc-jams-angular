810135dd40110404431cec31de2c0652109563cc
Jayson H Bucy <jayson.h.b@gmail.com>
2016-11-17 00:52:20 -0600

    checkpoint 5 controllers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,8 +9,14 @@
       $stateProvider
         .state('landing', {
           url: '/',
+		  controller: 'LandingCtrl as landing'
           templateUrl: 'templates/landing.html'
         })
+		.state('collection', {
+			url: '/collection',
+			controller: 'CollectionCtrl as collection',
+			templateUrl: '/templates/collection.html'
+		})
         .state('album', {
           url: '/album',
           templateUrl: 'templates/album.html'
