d8fae2da6fdd36fc77623125aa20904fa3f8b611
Jayson H Bucy <jayson.h.b@gmail.com>
2016-11-17 01:47:43 -0600

    assignment 5 controllers

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,7 +1,7 @@
 (function() {
      function CollectionCtrl() {
-		this.album = [];
-		for (var 1 = 0; i < 4; i++){
+		this.albums = [];
+		for (var i = 0; i < 4; i++){
 			this.albums.push(angular.copy(albumPicasso));
 		}
      }
